3cf290e7c1b48417f32250ed83c395c77e7663fe
Amanda <amands.miranda24@gmail.com>
2025-02-03 02:07:43 -0300

    centralizando botoes bot e sobre na home

--- src/app/(components)/utils/aboutbutton.tsx
+++ src/app/(components)/utils/aboutbutton.tsx
@@ -1,54 +1,31 @@
 'use client';
 import React from 'react';
+import { motion } from 'framer-motion';
 
-type AboutButtonProps = object;
-
-const AboutButton: React.FC<AboutButtonProps> = () => {
+const AboutButton: React.FC = () => {
   return (
-    <button
+    <motion.button
+      // initial={{ scale: 0.8 }}
+      // animate={{ scale: 1 }}
+      whileHover={{ scale: 1.1 }}
+      transition={{ duration: 0.1 }} // Transição rápida (mesma do botão do chatbot)
+      className="w-16 h-16 bg-transparent rounded-full border-4 border-[#ffde59] flex items-center justify-center shadow-xl focus:outline-none"
       style={{
-        position: 'fixed',
-        bottom: '20px',
-        left: '20px',
-        width: '50px',
-        height: '50px',
-        borderRadius: '50%',
-        border: '4px solid #ffde59',
-        backgroundColor: 'transparent',
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        cursor: 'pointer',
-        boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-        transition: 'transform 0.2s ease, box-shadow 0.2s ease',
-      }}
-      onMouseOver={(e) => {
-        e.currentTarget.style.transform = 'scale(1.1)';
-        e.currentTarget.style.boxShadow = '0 6px 8px rgba(0, 0, 0, 0.2)';
-      }}
-      onMouseOut={(e) => {
-        e.currentTarget.style.transform = 'scale(1)';
-        e.currentTarget.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1)';
-      }}
-      onMouseDown={(e) => {
-        e.currentTarget.style.transform = 'scale(0.98)';
-        e.currentTarget.style.boxShadow = '0 3px 5px rgba(0, 0, 0, 0.2)';
-      }}
-      onMouseUp={(e) => {
-        e.currentTarget.style.transform = 'scale(1.1)';
-        e.currentTarget.style.boxShadow = '0 6px 8px rgba(0, 0, 0, 0.2)';
+        position: 'fixed', // Fixa o botão na tela
+        bottom: '20px', // Alinhado na parte inferior
+        left: '20px', // Alinhado no canto inferior esquerdo
+        transition: 'transform 0.2s ease', // Suaviza o hover
       }}
     >
-      <span
-        style={{
-          fontSize: '24px',
-          fontWeight: 'bold',
-          color: '#ffde59',
-        }}
+      <motion.span
+        initial={{ scale: 1 }}
+        // whileHover={{ scale: 1.2 }} // Aumenta o ícone no hover
+        transition={{ duration: 0.2 }} // Transição rápida (mesma do botão do chatbot)
+        className="text-[#ffde59] text-3xl font-normal"
       >
         ?
-      </span>
-    </button>
+      </motion.span>
+    </motion.button>
   );
 };
 

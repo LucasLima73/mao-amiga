9c27118de95e62532b7f70416444e879d5ac2596
Amanda <amands.miranda24@gmail.com>
2025-02-03 02:28:30 -0300

    criacao pagina sobre do botao na home

--- src/app/(components)/utils/aboutbutton.tsx
+++ src/app/(components)/utils/aboutbutton.tsx
@@ -1,31 +1,26 @@
 'use client';
+
 import React from 'react';
-import { motion } from 'framer-motion';
+import { useRouter } from 'next/navigation';
 
 const AboutButton: React.FC = () => {
+  const router = useRouter();
+
   return (
-    <motion.button
-      // initial={{ scale: 0.8 }}
-      // animate={{ scale: 1 }}
-      whileHover={{ scale: 1.1 }}
-      transition={{ duration: 0.1 }} // Transição rápida (mesma do botão do chatbot)
-      className="w-16 h-16 bg-transparent rounded-full border-4 border-[#ffde59] flex items-center justify-center shadow-xl focus:outline-none"
+    <button
+      onClick={() => router.push('/sobre')}
+      className="w-16 h-16 bg-transparent rounded-full border-4 border-[#ffde59] flex items-center justify-center focus:outline-none"
       style={{
-        position: 'fixed', // Fixa o botão na tela
-        bottom: '20px', // Alinhado na parte inferior
-        left: '20px', // Alinhado no canto inferior esquerdo
-        transition: 'transform 0.2s ease', // Suaviza o hover
+        position: 'fixed',
+        bottom: '20px',
+        left: '20px',
+        transition: 'transform 0.2s ease',
       }}
+      onMouseOver={(e) => (e.currentTarget.style.transform = 'scale(1.1)')}
+      onMouseOut={(e) => (e.currentTarget.style.transform = 'scale(1)')}
     >
-      <motion.span
-        initial={{ scale: 1 }}
-        // whileHover={{ scale: 1.2 }} // Aumenta o ícone no hover
-        transition={{ duration: 0.2 }} // Transição rápida (mesma do botão do chatbot)
-        className="text-[#ffde59] text-3xl font-normal"
-      >
-        ?
-      </motion.span>
-    </motion.button>
+      <span className="text-[#ffde59] text-3xl font-normal">?</span>
+    </button>
   );
 };
 

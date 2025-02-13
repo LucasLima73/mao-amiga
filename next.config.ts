4277f3849fb29bd64cc3f9f8af97591a609b927e
LucasLima73 <75809895+LucasLima73@users.noreply.github.com>
2025-02-13 20:38:14 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true, // Ignorar o ESLint durante o build
   },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 };
 
 export default nextConfig;

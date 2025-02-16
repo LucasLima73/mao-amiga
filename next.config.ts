e02400d2060c49e2bd71d9a4a186bc70ad9548d1
Lucas Lima <lucaslavey@outlook.com>
2025-02-15 22:14:59 -0300

    add google analyticsclick events

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,36 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const cspHeader = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' https://www.googletagmanager.com https://www.google-analytics.com;
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' blob: data: https://www.google-analytics.com;
+  font-src 'self';
+  object-src 'none';
+  base-uri 'self';
+  form-action 'self';
+  frame-ancestors 'none';
+  upgrade-insecure-requests;
+`;
 
 const nextConfig: NextConfig = {
   eslint: {
-    ignoreDuringBuilds: true, // Ignorar o ESLint durante o build
+    ignoreDuringBuilds: true, // Ignora o ESLint durante o build
+  },
+  async headers() {
+    return [
+      {
+        // Aplica para todas as rotas
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            // Remove as quebras de linha para deixar a string em uma única linha
+            value: cspHeader.replace(/\n/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
   },
 };
 

f69cd726b59e98c48addb3b0fd031f02003e42ae
Lucas Lima <lucaslavey@outlook.com>
2025-02-15 22:08:08 -0300

    add google analyticsclick events

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,11 +26,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Carrega o script do Google Analytics com crossOrigin para ajudar no preload */}
+        {/* Carrega o script do Google Analytics */}
         <Script
           strategy="afterInteractive"
           src="https://www.googletagmanager.com/gtag/js?id=G-4JXWD7BCWV"
-          crossOrigin="anonymous"
         />
         {/* Inicializa o GA */}
         <Script id="ga-init" strategy="afterInteractive">

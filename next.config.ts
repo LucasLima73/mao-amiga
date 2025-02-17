0e83be2e5ed44cbda4a282a7da8778b8dbf49cb8
LucasLima73 <75809895+LucasLima73@users.noreply.github.com>
2025-02-17 19:19:40 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,5 @@
 import type { NextConfig } from "next";
 
-const cspHeader = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;
-  style-src 'self' 'unsafe-inline';
-  img-src 'self' blob: data: https://www.google-analytics.com;
-  font-src 'self';
-  object-src 'none';
-  connect-src 'self' https://identitytoolkit.googleapis.com https://www.google-analytics.com;
-  base-uri 'self';
-  form-action 'self';
-  frame-ancestors 'none';
-  upgrade-insecure-requests;
-`;
-
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -25,7 +11,19 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: cspHeader.replace(/\n/g, " ").trim(),
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;
+              style-src 'self' 'unsafe-inline';
+              img-src 'self' blob: data: https://www.google-analytics.com;
+              font-src 'self';
+              object-src 'none';
+              connect-src 'self' https://identitytoolkit.googleapis.com https://www.google-analytics.com;
+              base-uri 'self';
+              form-action 'self';
+              frame-ancestors 'none';
+              upgrade-insecure-requests;
+            `.replace(/\n/g, " ").trim(),
           },
         ],
       },

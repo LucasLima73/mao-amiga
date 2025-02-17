11f38fbf369a6c25f3838a923a1cb4a26bf802d0
LucasLima73 <75809895+LucasLima73@users.noreply.github.com>
2025-02-17 19:22:37 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,7 @@ const nextConfig: NextConfig = {
               img-src 'self' blob: data: https://www.google-analytics.com;
               font-src 'self';
               object-src 'none';
-              connect-src 'self' https://identitytoolkit.googleapis.com https://www.google-analytics.com;
+              connect-src 'self' https://identitytoolkit.googleapis.com https://www.google-analytics.com https://firestore.googleapis.com;
               base-uri 'self';
               form-action 'self';
               frame-ancestors 'none';

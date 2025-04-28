be71cfc0336627d9136dcf7b26059d53eec862d2
Amanda <amands.miranda24@gmail.com>
2025-04-27 22:30:27 -0300

    Ajustes no .env.example e next.config.ts antes de merge

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
@@ -13,17 +14,38 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://www.google-analytics.com;
+              script-src 
+                'self' 
+                'unsafe-inline' 
+                'unsafe-eval' 
+                https://www.googletagmanager.com 
+                https://www.google-analytics.com
+                https://maps.googleapis.com
+                https://maps.gstatic.com;
               style-src 'self' 'unsafe-inline';
-              img-src 'self' blob: data: https://www.google-analytics.com;
+              img-src 
+                'self' 
+                data: 
+                blob: 
+                https://www.google-analytics.com
+                https://maps.googleapis.com
+                https://maps.gstatic.com;
+              connect-src 
+                'self' 
+                https://identitytoolkit.googleapis.com 
+                https://www.google-analytics.com 
+                https://firestore.googleapis.com
+                https://maps.googleapis.com
+                https://maps.gstatic.com;
               font-src 'self';
               object-src 'none';
-              connect-src 'self' https://identitytoolkit.googleapis.com https://www.google-analytics.com https://firestore.googleapis.com;
               base-uri 'self';
               form-action 'self';
               frame-ancestors 'none';
               upgrade-insecure-requests;
-            `.replace(/\n/g, " ").trim(),
+            `
+              .replace(/\n/g, " ")
+              .trim(),
           },
         ],
       },
